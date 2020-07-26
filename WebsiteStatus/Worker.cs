0ce49290708643d123059b1881c74a9a0b50ce61
Mano Cardozo <manasses.cardozo@gmail.com>
2020-07-26 16:27:57 +1200

    Add worker

--- WebsiteStatus/Worker.cs
+++ WebsiteStatus/Worker.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
@@ -11,18 +9,41 @@ namespace WebsiteStatus
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private HttpClient client;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
         }
 
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            client = new HttpClient();
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            client.Dispose();
+            return base.StopAsync(cancellationToken);
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                var result = await client.GetAsync("https://www.google.com");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation($"The website is up. Status code {result.StatusCode}");
+                }
+                else
+                {
+                    _logger.LogError($"The website is down {result.StatusCode}");
+                }
+
+                await Task.Delay(5000, stoppingToken);
             }
         }
     }
